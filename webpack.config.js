27a6bf0fb9fdab750f5565e30c9d744556494d03
PoeticBliss <meremezeukah@gmail.com>
2016-09-30 15:34:57 -0400

    added more plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,25 @@
 const path = require('path');
+const webpack = require('webpack');
 const config = require('./package.json');
 
+const PROD = JSON.parse(process.env.PROD_ENV || '0');
+
 const WebpackBrowserPlugin = require('webpack-browser-plugin');
+
+let plugins = [];
+
+PROD ? [
+    plugins.push(new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false }
+    }))
+  ] : '';
 
 module.exports = {
   entry: path.resolve(__dirname, config.main),
   devtools: "source_map",
   output: {
     path: __dirname,
-    filename: "build/bundle.js"
+    filename: PROD ? "build/keybinder.min.js" : "build/keybinder.js"
   },
   module: {
     loaders: [
@@ -22,9 +33,9 @@ module.exports = {
   resolve: {
     extensions: ['', '.js', '.es6']
   },
-  plugins: [
+  plugins: plugins.push(
     new WebpackBrowserPlugin({
       browser: "Chrome"
     })
-  ]
+  )
 }
