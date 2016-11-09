5f1049b74132a79036b69116cb337d475f0154ca
PoeticBliss <meremezeukah@gmail.com>
2016-11-09 17:15:00 -0500

    added library name

--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,22 @@ const config = require('./package.json');
 
 const PROD = (process.env.NODE_ENV === 'production');
 
-const WebpackBrowserPlugin = require('webpack-browser-plugin');
+console.log(PROD);
+
+// const WebpackBrowserPlugin = require('webpack-browser-plugin');
+// plugins.push(
+//   new WebpackBrowserPlugin({
+//     browser: "Chrome"
+//   })
+// )
+
 
 let plugins = [];
 
-PROD ? [
-    plugins.push(new webpack.optimize.UglifyJsPlugin({
+PROD ? plugins.push(new webpack.optimize.UglifyJsPlugin({
       compress: { warnings: false }
     }))
-  ] : '';
+  : '' ;
 
 module.exports = {
   entry: path.resolve(__dirname, config.entry),
@@ -20,6 +27,7 @@ module.exports = {
   output: {
     path: __dirname,
     filename: PROD ? "build/keybinder.min.js" : "build/keybinder.js",
+    library: "keybinder",
     libraryTarget: "umd"
   },
   module: {
@@ -34,9 +42,5 @@ module.exports = {
   resolve: {
     extensions: ['', '.js', '.es6']
   },
-  plugins: plugins.push(
-    new WebpackBrowserPlugin({
-      browser: "Chrome"
-    })
-  )
+  plugins: plugins
 }
